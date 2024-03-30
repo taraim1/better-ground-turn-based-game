023b3ec940a90dd514406cad05b980696a0b9300
taraim1 <kimjeyul200426@gmail.com>
2024-03-30 20:44:59 +0900

    전투 개발중
    1. 전투 시작시 파티 데이터를 텍스트 파일에서 불러오게 함
    2. 전투 시작시 생성되는 플레이어 오브젝트의 이름이 파티 데이터를 따라 바뀌도록 함

--- Assets/Script/Managers/BattleManager.cs
+++ Assets/Script/Managers/BattleManager.cs
@@ -12,6 +12,14 @@ public class BattleManager : Singletone<BattleManager> //싱글톤임
     public bool battle_start_trigger = false; // 전투를 시작시키는 트리거
     public bool is_Characters_spawned = false; // 전투 시작시 플레이이어와 적 캐릭터가 스폰되었는가?
     public bool is_Characters_loaded = false; // 전투 시작시 캐릭터 데이터가 불러와졌는가?
+
+    public Vector3[] playable_character_position_settings = new Vector3[4]; //플레이어블 캐릭터 스폰 위치
+
+    //전투 중인 캐릭터의 데이터 인스턴스 리스트
+    public List<Playable_Character> playable_Characters_data = new List<Playable_Character>();
+
+    //전투 중인 캐릭터의 게임오브젝트 리스트
+    public List<GameObject> playable_characters = new List<GameObject>();
     public enum phases // 한 턴의 페이즈 모음
     { 
         turn_start_effect_phase,
@@ -25,9 +33,26 @@ public class BattleManager : Singletone<BattleManager> //싱글톤임
     {
         //변수 초기화
         is_Characters_spawned = false;
+        playable_Characters_data.Clear();
+        playable_characters.Clear();
 
-        //캐릭터 생성
+        //파티 데이터 불러오기
+        PartyManager.instance.read_Json_file();
+
+        //전투 중인 캐릭터의 데이터 리스트 불러오기
+        for (int i = 0; i < PartyManager.party_member_count; i++) 
+        { 
+            playable_Characters_data.Add(PartyManager.instance.get_character_of_party(i));
+        }
+ 
+        //캐릭터 오브젝트 생성
         CharacterManager.instance.spawn_character();
+
+        //캐릭터 오브젝트 이름 설정
+        for (int i = 0; i < PartyManager.party_member_count; i++) 
+        {
+            playable_characters[i].name = playable_Characters_data[i].get_character_name();
+        }
 
         yield break;
     
--- Assets/Script/Managers/CharacterManager.cs
+++ Assets/Script/Managers/CharacterManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class CharacterManager : Singletone<CharacterManager>
 {
@@ -16,30 +17,27 @@ public class CharacterManager : Singletone<CharacterManager>
 
     public GameObject playable_character_base;
 
-    public int playable_character_spawn_count = 0; //플레이어블 캐릭터가 몇 개 스폰되었는지 저장
 
-    
-    
-    public Vector3[] playable_character_position_settings = new Vector3[4] { // 플레이어블 캐릭터 위치 설정
-        new Vector3(-8, 2, 0),
-        new Vector3(-8, -3, 0),
-        new Vector3(-3, 2, 0),
-        new Vector3(-3, -3, 0)
-    };
 
 
     public List<GameObject> playerble_Characters = new List<GameObject>(); // 플레이어블 캐릭터들 리스트
 
     public void spawn_character()
     {
-
-        //플레이어블 캐릭터 생성
-        for (int i = 0; i < 4; i++) 
+       for (int i = 0; i < 4; i++) 
         {
-            
-            SpawnCharactor.instance.spawn_playerble_charactor(playable_character_base);
-            
+
+            //플레이어블 캐릭터가 소환될 위치를 불러옴
+            Vector3 spawn_pos = BattleManager.instance.playable_character_position_settings[i];
+
+            //플레이어블 캐릭터 생성
+            GameObject obj = Instantiate(playable_character_base, spawn_pos, Quaternion.identity);
+
+            //플레이어블 캐릭터 오브젝트를 BattleManager의 리스트에 넣기
+            BattleManager.instance.playable_characters.Add(obj);
+
         }
+
         BattleManager.instance.is_Characters_spawned = true;
     }
 }
