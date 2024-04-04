da6b7f927fd40d4944d1c82dfa2a2d07bda25d3d
taraim1 <kimjeyul200426@gmail.com>
2024-04-05 01:07:07 +0900

    버그 픽스 및 기능 추가
    1. Main씬으로 전환시 Managers가 중복생성되는 버그 수정
    2. Battle씬으로 전환 시 전투가 시작되도록 함
    3. 파티 데이터 저장 형식을 .txt에서 .json으로 변경

--- Assets/Script/Singletone/Singletone.cs
+++ Assets/Script/Singletone/Singletone.cs
@@ -9,6 +9,7 @@ public class Singletone<T> : MonoBehaviour where T : MonoBehaviour //싱글톤 만드
 
     private void Awake()
     {
+
         if (instance == null)
         {
             instance = (T)FindObjectOfType(typeof(T));
@@ -18,6 +19,11 @@ public class Singletone<T> : MonoBehaviour where T : MonoBehaviour //싱글톤 만드
                 GameObject obj = new GameObject(typeof(T).Name, typeof(T));
                 instance = obj.GetComponent<T>();
             }
+        }
+        else 
+        {
+            //인스턴스 존재시 게임오브젝트 파괴
+            Destroy(gameObject);
         }
         DontDestroyOnLoad(this.transform.root.gameObject);        
     }
