4de35fe3b3151d7ae90de4e9484bb88418456cb0
taraim1 <kimjeyul200426@gmail.com>
2024-04-12 23:23:23 +0900

    자원 시스템 수정
    public 변수로 get set 활용하게 함 (간단해짐)

--- Assets/Script/test.cs
+++ Assets/Script/test.cs
@@ -7,9 +7,25 @@ public class test : MonoBehaviour
 
     private void Start()
     {
-        ResourceManager.instance.get_resource(ResourceManager.resources.gold); //골드 값 가져오기
-        ResourceManager.instance.add_resource(ResourceManager.resources.gold, 10); //골드 더하기
-        //ResourceManager에 있는 다른 메소드들은 신경 안 써도 되게 만들어놓음 위에것만 쓰면 됨
+        print(ResourceManager.instance.Gold); //골드 값 가져와서 콘솔로 출력
+
+        ResourceManager.instance.Gold = 100; //골드 설정하기
+        print(ResourceManager.instance.Gold);
+
+        ResourceManager.instance.Gold = -100; //이건 음수라서 적용 안 됨
+
+        //ResourceManager.instance.Gold를 변수처럼 쓰면 됨
+
+
+        /* 
+            ResourceManager에서 지원하는 기능 목록 
+            
+            1. 골드 값을 json파일로 저장 (자동으로 되도록 만들어놨으니 신경x 해도 됨)
+            2. 골드 값을 json파일에서 불러오기 (자동으로 됨)
+            3. 골드 값을 변수처럼 사용하기
+            4. 골드 값이 음수가 되는 걸 자동으로 막기
+              
+         */
     }
 
 }
