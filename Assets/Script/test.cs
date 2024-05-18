1fe67ccb1c786e0c9718d95ca3c72bf1f1fe59d4
taraim1 <kimjeyul200426@gmail.com>
2024-05-18 22:09:58 +0900

    버전 맞춤
    "

--- Assets/Script/test.cs
+++ Assets/Script/test.cs
@@ -1,17 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
-using UnityEngine.Rendering;
 
 public class test : MonoBehaviour
 {
 
-    private void Start()
-    {
-
-
-    }
 
 }
 
