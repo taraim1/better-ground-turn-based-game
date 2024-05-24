839af341f8331fa00bbfc0224f3528e4217a8ef3
남현우 <164761964+nhw2075@users.noreply.github.com>
2024-05-24 15:52:05 +0900

    스킬카드기능 테스트용
    "

--- Assets/Script/test.cs
+++ Assets/Script/test.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
 
-public class test : MonoBehaviour
+public class test : MonoBehaviour, IPointerDownHandler
 {
+    public TMP_Text tmp;
+   	public CardManager.skillcard_code code;
+	[SerializeField] 
+	CardsSO cardso;
+	public Cards carddata;
 
-
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        carddata = cardso.cards[(int)code];
+        tmp.text = carddata.name;
+    }
+    
 }
 
 
