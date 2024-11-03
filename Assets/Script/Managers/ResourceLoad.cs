579663a79b103082db716178a380d091b037e89d
hyeonseoi <162105222+hyeonseoi@users.noreply.github.com>
2024-11-03 19:19:06 +0900

    123

--- Assets/Script/Managers/ResourceLoad.cs
+++ Assets/Script/Managers/ResourceLoad.cs
@@ -5,12 +5,12 @@ using TMPro;
 public class ResourceLoad : MonoBehaviour
 {
     public TextMeshProUGUI GoldText;
-    public TextMeshProUGUI GemText;
-    public TextMeshProUGUI WaterText;
+    //public TextMeshProUGUI GemText;
+    //public TextMeshProUGUI WaterText;
     void Update()
     {
         GoldText.text = "골드 : " +  ResourceManager.instance.Gold;
-        GemText.text = "보석 : " +  ResourceManager.instance.Gem;
-        WaterText.text = "성수 : " +  ResourceManager.instance.Water;
+        //GemText.text = "보석 : " +  ResourceManager.instance.Gem;
+        //WaterText.text = "성수 : " +  ResourceManager.instance.Water;
     }
 }
